93da2523a0b3dfd5326b3aaecf33b4678069bdf0
KajeA <59009825+KajeA@users.noreply.github.com>
2023-12-08 12:04:23 -0700

    Updated wounds calculator

--- app/src/main/java/com/example/test2/AttackFragment.java
+++ app/src/main/java/com/example/test2/AttackFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -29,6 +32,41 @@ public class AttackFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        EditText strengthInput = view.findViewById(R.id.strength_input);
+        EditText toughnessInput = view.findViewById(R.id.toughness_input);
+        Button calculate = view.findViewById(R.id.calculate_wound_button);
+        TextView woundsMessageOutput = view.findViewById(R.id.wounds_message_output);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int strength = Integer.parseInt(strengthInput.getText().toString());
+                int toughness = Integer.parseInt(toughnessInput.getText().toString());
+                String wounds_message = "";
+
+            // if strength is double toughness 2+
+            // if strength is greater than toughness 3+
+            // if strength is equal to toughness 4+
+            // if strength is less than toughness 5+
+            // if strength is half toughness 6+
+            if (strength >= 2 * toughness) {
+                wounds_message = getString(R.string.wounds_message_double);
+            } else if (strength > toughness) {
+                wounds_message = getString(R.string.wounds_message_greater);
+            } else if (strength == toughness) {
+                wounds_message = getString(R.string.wounds_message_equal);
+            } else if (strength < toughness) {
+                wounds_message = getString(R.string.wounds_message_less);
+            } else if (strength <= toughness / 2)
+                wounds_message = getString(R.string.wounds_message_half);
+            }
+
+            woundsMessageOutput.setText(wounds_message);
+
+        });
+    };
+
+
 //        view.findViewById(R.id.goto_attack_button).setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -36,7 +74,7 @@ public class AttackFragment extends Fragment {
 //                        .navigate(R.id.action_ShootingFragment_to_AttackFragment);
 //            }
 //        });
-    }
+
 
     @Override
     public void onDestroyView() {
